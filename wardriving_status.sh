6f26f80ad82938fd32811a208dab082f0a6da9d8
TME520 HP14 <tme520@gmail.com>
2020-09-05 20:06:32 +1000

    New feature: GPS sources count

--- wardriving_status.sh
+++ wardriving_status.sh
@@ -4,9 +4,24 @@ while true
 do
 	# Refreshing data
 	BATSTAT=$(acpi -b | awk '{print $3}' | tr -d ',')
-	BATCHARGE=$(acpi -b | awk '{print $4}' | tr -d '%')
+	BATCHARGE=$(acpi -b | awk '{print $4}' | tr -d ',' | tr -d '%')
 	TEMP=$(acpi -t | awk '{print $1" "$2" "$3}' | tr [,] [.])
 	KISMETCOUNT=$(ps -ef|grep -w [k]ismet|wc -l)
+	(echo "open 127.0.0.1 2947" && sleep 2 && echo "?POLL;") | telnet > /tmp/magoo
+	grep active /tmp/magoo > /tmp/magoo2
+	if [ $? -eq 0 ]
+	then
+		GPSFIX=$(cat /tmp/magoo2 | awk -F ',' '{print $3}' | awk -F ':' '{print $2}')
+	else
+		GPSFIX="Failed to get GPS status."
+	fi
+
+	# DEBUG
+	echo "[DEBUG] BATSTAT="$BATSTAT
+	echo "[DEBUG] BATCHARGE="$BATCHARGE
+	echo "[DEBUG] TEMP="$TEMP
+	echo "[DEBUG] KISMETCOUNT="$KISMETCOUNT
+	echo "[DEBUG] GPSFIX="$GPSFIX
 
 	# Preparing output
 	echo "Status report." > /tmp/tts
@@ -17,12 +32,14 @@ do
 	if [ $BATCHARGE -lt 11 ]
 	then
 		echo "Warning: Battery is low." >> /tmp/tts
-	elif [ $BATCHARGE -lt 6 ]
+	elif [ $BATCHARGE -lt 666 ]
 	then
 		echo "Warning: Battery is very low. System will shutdown shortly." >> /tmp/tts
 	fi
 	echo "Temperature. "$TEMP >> /tmp/tts
 	echo "" >> /tmp/tts
+	echo "Active GPS sources." >> /tmp/tts
+	echo $GPSFIX"." >> /tmp/tts
 	echo "Kismet count. "$KISMETCOUNT"." >> /tmp/tts
 	echo "" >> /tmp/tts
 	if [ "$KISMETCOUNT" == "0" ]
