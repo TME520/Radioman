7a0ab720687af132ed704323dca3ca413f447d81
TME520 HP14 <tme520@gmail.com>
2020-09-10 01:21:53 +1000

    New feature: check if the wifi sources configured in Kismet are UP.

--- wardriving_status.sh
+++ wardriving_status.sh
@@ -3,28 +3,41 @@
 while true
 do
 	# Refreshing data
-	grep '^source=' /etc/kismet/kismet.conf | awk -F '=' '{print $2}' | awk -F ':' '{print $1}' > /tmp/if2check
 	PBCOUNT=0
+	BATSTAT=$(acpi -b | awk '{print $3}' | tr -d ',')
+	KISMETSRCCNT=$(grep -c '^source=' /etc/kismet/kismet.conf)
+	if [ $KISMETSRCCNT -gt 0 ]
+	then
+		grep '^source=' /etc/kismet/kismet.conf | awk -F '=' '{print $2}' | awk -F ':' '{print $1}' > /tmp/if2check
+	fi
 	BATSTAT=$(acpi -b | awk '{print $3}' | tr -d ',')
 	BATCHARGE=$(acpi -b | awk '{print $4}' | tr -d ',' | tr -d '%')
 	TEMP=$(acpi -t | awk '{print $1" "$2" "$3}' | tr [,] [.])
 	KISMETCOUNT=$(ps -ef|grep -w [k]ismet|wc -l)
 	gpspipe -n 30 -r -x 15 > /tmp/magoo
-	grep GPGSV /tmp/magoo > /tmp/magoo2
-	SATINVIEW=$(tail -1 /tmp/magoo2 | awk -F ',' '{print $4}')
-	grep GPGSA /tmp/magoo > /tmp/magoo2
-	SATFIX=$(tail -1 /tmp/magoo2 | awk -F ',' '{print $3}')
+	SATINVIEW="Unknown"
+	SATINVIEWCNT=$(grep -c GPGSV /tmp/magoo)
+	if [ $SATINVIEWCNT -gt 0 ]
+	then
+		grep GPGSV /tmp/magoo > /tmp/magoo2
+		SATINVIEW=$(tail -1 /tmp/magoo2 | awk -F ',' '{print $4}')
+	fi
 	SATFIXTYPE="Unknown"
-	if [ $SATFIX -eq 1 ]
+	SATFIXCNT=$(grep -c GPGSA /tmp/magoo)
+	if [ $SATFIXCNT -gt 0 ]
 	then
-		SATFIXTYPE="No fix. Warning: no GPS location currently available."
-		((PBCOUNT++))
-	elif [ $SATFIX -eq 2 ]
-	then
-		SATFIXTYPE="2D fix."
-	elif [ $SATFIX -eq 3 ]
-	then
-		SATFIXTYPE="3D fix."
+		grep GPGSA /tmp/magoo > /tmp/magoo2
+		SATFIX=$(tail -1 /tmp/magoo2 | awk -F ',' '{print $3}')
+		if [ $SATFIX -eq 1 ]
+		then
+			SATFIXTYPE="No fix."
+		elif [ $SATFIX -eq 2 ]
+		then
+			SATFIXTYPE="2D fix."
+		elif [ $SATFIX -eq 3 ]
+		then
+			SATFIXTYPE="3D fix."
+		fi
 	fi
 
 	# DEBUG
@@ -35,28 +48,59 @@ do
 	echo "[DEBUG] SATINVIEW="$SATINVIEW
 	echo "[DEBUG] SATFIX="$SATFIX
 	echo "[DEBUG] SATFIXTYPE="$SATFIXTYPE
+	echo "[DEBUG] SATFIXCNT="$SATFIXCNT
+	echo "[DEBUG] KISMETSRCCNT="$KISMETSRCCNT
 
 	# Preparing output
+	# Metrics
 	echo "Status report." > /tmp/tts
 	echo "Battery." >> /tmp/tts
 	echo $BATSTAT"." >> /tmp/tts
 	echo $BATCHARGE"%." >> /tmp/tts
 	echo "" >> /tmp/tts
+	echo "Temperature. "$TEMP >> /tmp/tts
+	echo "" >> /tmp/tts
+	echo "GPS information." >> /tmp/tts
+	echo "Number of GPS satellites in view: "$SATINVIEW". GPS fix type is "$SATFIXTYPE"." >> /tmp/tts
+	echo "Kismet count. "$KISMETCOUNT"." >> /tmp/tts
+	echo "" >> /tmp/tts
+
+	# Announcing issues
 	if [ $BATCHARGE -lt 11 ]
 	then
 		echo "Warning: Battery is low." >> /tmp/tts
 		((PBCOUNT++))
 	fi
-	echo "Temperature. "$TEMP >> /tmp/tts
-	echo "" >> /tmp/tts
-	echo "GPS information." >> /tmp/tts
-	echo $SATINVIEW" GPS satellites in view. GPS fix type is "$SATFIXTYPE"." >> /tmp/tts
-	echo "Kismet count. "$KISMETCOUNT"." >> /tmp/tts
-	echo "" >> /tmp/tts
-	if [ "$KISMETCOUNT" == "0" ]
+	if [ $SATFIXCNT -eq 0 ]
+	then
+		echo "Warning: No GPS location is being recorded." >> /tmp/tts 
+		((PBCOUNT++))
+	fi
+	if [ $SATINVIEWCNT -eq 0 ]
+	then
+		echo "Warning: No GPS satellites in view." >> /tmp/tts 
+		((PBCOUNT++))
+	fi
+	if [ $KISMETCOUNT -eq 0 ]
 	then
 		echo "Warning: Kismet is not running. I repeat, kismet is not running." >> /tmp/tts
 		((PBCOUNT++))
+	fi
+	if [ $KISMETSRCCNT -eq 0 ]
+	then
+		echo "Warning: No sources configured in Kismet. Wireless networks are not getting mapped." >> /tmp/tts
+		((PBCOUNT++))
+	else
+		while read wifi_interface
+		do
+			WIFIIFSTATUS=$(iwconfig $wifi_interface 2>&1 | grep -c "No such device")
+			echo "WIFIIFSTATUS="$WIFIIFSTATUS
+			if [ $WIFIIFSTATUS -gt 0 ]
+			then
+				echo "Warning: wireless interface DOWN." >> /tmp/tts
+				((PBCOUNT++))
+			fi
+		done < /tmp/if2check
 	fi
 
 	if [ $PBCOUNT -eq 0 ]
